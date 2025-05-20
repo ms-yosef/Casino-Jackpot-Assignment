78b8f448370d9dabee6cd7ffa1b5f3428491bd50
Yosef Trrachtenberg <yosef.trrachtenberg@gmail.com>
2025-05-20 08:25:36 +0300

    CORS middleware added

---
+++ packages/server/src/Middleware/CorsMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Casino\Server\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+/**
+ * Middleware for handling CORS
+ */
+class CorsMiddleware implements MiddlewareInterface
+{
+    /**
+     * Process an incoming server request and return a response
+     *
+     * @param Request $request The request
+     * @param RequestHandler $handler The handler
+     * @return Response The response
+     */
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
+    }
+}
